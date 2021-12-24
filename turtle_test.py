276a67455bbed7dd82643d4635eca0bd5d21cf87
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-12-24 22:31:45 +0530

    commit

---
+++ turtle_test.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.showturtle()
+turtle.write("Welcome to python")
+turtle.forward(100)
+turtle.right(90)
+turtle.color("red")
+turtle.forward(50)
+turtle.right(90)
+turtle.color("green")
+turtle.forward(100)
+turtle.right(45)
+turtle.color("red")
+turtle.forward(80)
+
