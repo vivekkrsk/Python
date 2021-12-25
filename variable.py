08d432d0b2b674b0915d67dc4d374dab9e4d8470
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-12-25 21:21:23 +0530

    commit

---
+++ variable.py
@@ -0,0 +1,19 @@
+a="vivek"
+b=10
+c=29.3
+d=True
+e=None
+
+# printing the variable
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# printing the type of variable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
