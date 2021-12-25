97c7c0163b810c49d131a45d6f4704061cb4cdb9
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2021-12-25 21:20:46 +0530

    commit

---
+++ operators.py
@@ -0,0 +1,41 @@
+a=34
+b=45
+
+# Arithmetic operator
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# Assignment operator
+a=b
+print(a)
+a+=b
+print(a)
+a-=b
+print(a)
+a*=4
+print(a)
+a/=4
+print(a)
+
+#comparision operator
+b=(14>14)
+print(b)
+b=(14>=14)
+print(b)
+b=(14<14)
+print(b)
+b=(14<=14)
+print(b)
+b=(14==14)
+print(b)
+b=(14!=14)
+print(b)
+
+#Logical operator
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not b)
