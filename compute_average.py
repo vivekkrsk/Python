7edab6531dff94d9c6af565c8ef1986e957a133f
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-01-10 19:10:38 +0530

    commit

---
+++ compute_average.py
@@ -0,0 +1,8 @@
+# Compute average
+number1 = eval(input("Enter first number: "))
+number2 = eval(input("Enter second number: "))
+number3 = eval(input("Enter third number: "))
+
+average = (number1+number2+number3)/3
+
+print("The average of",number1,number2,number3,"is",average)
