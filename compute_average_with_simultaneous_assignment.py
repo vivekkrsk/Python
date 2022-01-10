487d3a4d34cb1de9e2d807bb7d7b5705c9be7117
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-01-10 19:13:44 +0530

    commit

---
+++ compute_average_with_simultaneous_assignment.py
@@ -0,0 +1,6 @@
+# Comput Average With Simultaneous Assignment
+
+number1,number2,number3 = eval(input("Enter Three number seperated by Commas:"))
+average = (number1+number2+number3)/3
+
+print("The average of",number1,number2,number3,"is",average)
