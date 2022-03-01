eb50807f44c42481052e26279da675d1ac02a58f
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-03-01 07:02:44 +0530

    commit

---
+++ print_grade_function.py
@@ -0,0 +1,19 @@
+def print_grade(score):
+    if score >= 90.0:
+        print("A")
+    elif score >= 80.0:
+        print("B")
+    elif score >= 70.0:
+        print('C')
+    elif score >= 60.0:
+        print("D")
+    else:
+        print("F")
+
+
+def main():
+    score = eval(input("Enter a score: "))
+    print("The grade is ", end='')
+    print_grade(score)
+
+main()
