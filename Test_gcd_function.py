87c36588e2b601d575e873e8fac35ad91ad549cd
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-03-02 07:03:37 +0530

    commit

---
+++ Test_gcd_function.py
@@ -0,0 +1,6 @@
+from gcdfunction import gcd
+
+n1 = eval(input("Enter the first integer: "))
+n2 = eval(input("Enter the Second integer: "))
+
+print("the greatest common divisor for", n1, "and",n2,"is",gcd(n1,n2))
