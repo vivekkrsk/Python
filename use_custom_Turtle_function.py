2a90a2e8a5afe11ee65cbae03c8e754d85708e57
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-03-02 07:04:14 +0530

    commit

---
+++ use_custom_Turtle_function.py
@@ -0,0 +1,15 @@
+import turtle
+from useful_turtle_function import *
+
+drawLine(-50,-50,50,50)
+
+writeText("Testing useful turtle function",-50,-50)
+
+drawPoint(0,0)
+
+drawCircle(0,0,80)
+
+drawRectangle(0,0,60,40)
+
+turtle.hideturtle()
+turtle.done()
